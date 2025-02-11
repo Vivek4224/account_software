5d4f6b6a525c8b7fc181f6763b8b4c959ad79a26
Vivek4224 <vivekbarvaliya4224@gmail.com>
2025-02-11 17:53:34 +0530

    dashboard complate

--- dashboard.py
+++ dashboard.py
@@ -33,7 +33,32 @@ class IMS:
         self.icon_side=PhotoImage(file="images/side.png")
 
         lbl_menu=Label(LeftMenu,text="Menu",font=("times new roman",20),bg="#009688").pack(side=TOP,fill=X)
-        btn_employee=Button(LeftMenu,text="Employee",image=self.icon_side,compound=LEFT,padx=20,font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+
+        btn_employee=Button(LeftMenu,text="Employee",image=self.icon_side,compound=LEFT,padx=5,font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_supplier=Button(LeftMenu,text="Supplier",image=self.icon_side,compound=LEFT,padx=5,font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_category=Button(LeftMenu,text="Category",image=self.icon_side,compound=LEFT,padx=5,font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_product=Button(LeftMenu,text="Product",image=self.icon_side,compound=LEFT,padx=5,font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_sales=Button(LeftMenu,text="Sales",image=self.icon_side,compound=LEFT,padx=5,font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_exit=Button(LeftMenu,text="Exit",image=self.icon_side,compound=LEFT,padx=5,font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+
+         #=== content ===
+        self.lbl_employee=Label(self.root,text="Total Employee\n[ 0 ]",bd=5,relief=RIDGE,bg="#33bbf9",fg="white",font=("goudy old style",20,"bold"))
+        self.lbl_employee.place(x=300,y=120,height=150,width=300)
+
+        self.lbl_supplier=Label(self.root,text="Total Supplier\n[ 0 ]",bd=5,relief=RIDGE,bg="#ff5722",fg="white",font=("goudy old style",20,"bold"))
+        self.lbl_supplier.place(x=650,y=120,height=150,width=300)
+
+        self.lbl_category=Label(self.root,text="Total category\n[ 0 ]",bd=5,relief=RIDGE,bg="#009688",fg="white",font=("goudy old style",20,"bold"))
+        self.lbl_category.place(x=1000,y=120,height=150,width=300)
+
+        self.lbl_product=Label(self.root,text="Total Product\n[ 0 ]",bd=5,relief=RIDGE,bg="#607d8b",fg="white",font=("goudy old style",20,"bold"))
+        self.lbl_product.place(x=300,y=300,height=150,width=300)
+
+        self.lbl_sales=Label(self.root,text="Total Sales\n[ 0 ]",bd=5,relief=RIDGE,bg="#ffc107",fg="white",font=("goudy old style",20,"bold"))
+        self.lbl_sales.place(x=650,y=300,height=150,width=300)
+
+         #=== Footer ===
+        lbl_footer=Label(self.root,text="Yogi Fashion | Developed By Vivek\nFor any Technical Issue Contact: 9924544012",font=("times new roman",12),bg="#4d636d",fg="white").pack(side=BOTTOM,fill=X)
 
 root=Tk()
 obj=IMS(root)
